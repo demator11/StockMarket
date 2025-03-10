77e018e4d29fcf18749735cb1718c4e8676a7177
Dema <toropovdema@gmail.com>
2025-03-10 14:36:42 +0500

    Create do deposit function

--- routers/balance.py
+++ routers/balance.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Header
+from models.ok import Ok
 
 router_balance = APIRouter()
 
@@ -8,3 +9,9 @@ def get_balance(api_key: str = Header(default=None)):
     # берем ключ авторизации из заголовка и по нему достаем баланс
 
     return {"MEMCOIN": 0, "DODGE": 100500}
+
+
+@router_balance.get("/api/v1/balance/deposit", summary="Deposit")
+def do_deposit(api_key: str = Header(default=None)) -> Ok:
+    # пытаемся положить на баланс юзера коины
+    return Ok()
