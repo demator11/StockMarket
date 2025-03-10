06b341ee9041a41a119e0f95cfd72a4735378b09
Dema <toropovdema@gmail.com>
2025-03-10 14:19:43 +0500

    Create balance get function

--- routers/balance.py
+++ routers/balance.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 
 router_balance = APIRouter()
 
 
-@router_balance.get("/api/v1/balance/")
-def balance():
-    return {"success": "True"}
+@router_balance.get("/api/v1/balance/", summary="Get Balance")
+def get_balance(api_key: str = Header(default=None)):
+    # берем ключ авторизации из заголовка и по нему достаем баланс
+
+    return {"MEMCOIN": 0, "DODGE": 100500}
