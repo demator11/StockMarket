3f926147368c8679ab4fc1e755b3e9d6ad5c6071
Dema <toropovdema@gmail.com>
2025-03-10 13:10:19 +0500

    Create orderbook class

--- routers/public.py
+++ routers/public.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from models.user import NewUser, User
 from models.instrument import Instrument
+from models.orderbook import L2OrderBook
 
 router_public = APIRouter()
 
@@ -19,3 +20,14 @@ def get_instrument_list() -> Instrument:
     # берем какие то там инструменты и отдаем их списком
     instrument = Instrument(name="string", ticker="TICKER")
     return instrument
+
+
+@router_public.get(
+    "/api/v1/public/orderbook/{ticker}", summary="Get Orderbook"
+)
+def get_orderbook(ticker: str, qty: int = 10) -> L2OrderBook:
+    # достаем список из qty бидов и асков по нужному ticker
+    bid_levels = [{"price": 1, "qty": 1}, {"price": 2, "qty": 2}]
+    ask_levels = [{"price": 3, "qty": 3}, {"price": 4, "qty": 4}]
+    result = L2OrderBook(bid_levels=bid_levels, ask_levels=ask_levels)
+    return result
