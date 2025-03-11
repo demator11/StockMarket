3ed192d4af3ecc80bffcd9dabadd6939e71c0f9f
Dema <toropovdema@gmail.com>
2025-03-11 14:21:59 +0500

    Create order response function

---
+++ models/order.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
+class Direction(Enum):
+    buy = "BUY"
+    sell = "SELL"
+
+
+class OrderStatus(Enum):
+    new = "NEW"
+    executed = "EXECUTED"
+    partially_executed = "PARTIALLY_EXECUTED"
+    cancelled = "CANCELLED"
+
+
+class LimitOrderBody(BaseModel):
+    direction: Direction
+    ticker: str
+    qry: int = Field(ge=1)
+    price: int = Field(gt=0)
+
+
+class LimitOrder(BaseModel):
+    id: UUID
+    status: OrderStatus
+    user_id: UUID
+    body: LimitOrderBody
+    filled: int = 0
+
+
+class CreateOrderResponse(BaseModel):
+    success: bool = True
+    order_id: int
--- routers/order.py
+++ routers/order.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Header
+
+from models.order import LimitOrderBody, CreateOrderResponse
 
 router_order = APIRouter()
 
 
 @router_order.get("/api/v1/order/")
-def order():
-    return {"success": "True"}
+def create_order_response(
+    new_order: LimitOrderBody, api_key: Annotated[str | None, Header()] = None
+) -> CreateOrderResponse:
+    # создаем order и присваиваем id
+    order_id = 123
+    return CreateOrderResponse(order_id=order_id)
