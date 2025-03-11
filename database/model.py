47b96567553b32c9f1770dd28bd8fe0401432470
Dema <toropovdema@gmail.com>
2025-03-11 20:55:51 +0500

    Create balances database

--- database/model.py
+++ database/model.py
@@ -25,3 +25,14 @@ class OrdersOrm(Base):
     direction: Mapped[Direction]
     ticker: Mapped[str]
     qty: Mapped[int]
+
+
+class BalancesOrm(Base):
+    __tablename__ = "balances"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE")
+    )
+    currency: Mapped[str]
+    qty: Mapped[int]
