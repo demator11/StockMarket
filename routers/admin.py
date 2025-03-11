038b3c9c7c911059b20094a07717204ec970dd8f
Dema <toropovdema@gmail.com>
2025-03-11 14:44:28 +0500

    Create admin instruments

--- routers/admin.py
+++ routers/admin.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Header
+
+from models.instrument import Instrument
+from models.ok import Ok
 
 router_admin = APIRouter()
 
 
-@router_admin.get("/api/v1/admin/")
-def admin():
-    return {"success": "True"}
+@router_admin.post("/api/v1/admin/instrument")
+def add_instrument(
+    instrument: Instrument,
+    authorization: Annotated[str | None, Header()] = None,
+) -> Ok:
+    # добавляем инструмент
+    return Ok()
+
+
+@router_admin.delete("/api/v1/admin/instrument/{ticker}")
+def delete_instrument(
+    ticker: str, authorization: Annotated[str | None, Header()] = None
+) -> Ok:
+    # удаляем инструмент
+    return Ok()
