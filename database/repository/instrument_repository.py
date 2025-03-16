ff1b72daba3a7baacd4961dd69f4cb22fe9765ef
Dema <toropovdema@gmail.com>
2025-03-16 20:56:52 +0500

    Add init class to balance repository

--- database/repository/instrument_repository.py
+++ database/repository/instrument_repository.py
@@ -1,38 +1,39 @@
 from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.instrument import Instrument
 from database.database_models.instrument import InstrumentOrm
-from database.engine import async_session_factory
 
 
 class InstrumentRepository:
-    @staticmethod
-    async def create_instrument(new_instrument: Instrument) -> Instrument:
-        async with async_session_factory.begin() as session:
-            result = await session.scalars(
-                insert(InstrumentOrm)
-                .values(
-                    name=new_instrument.name,
-                    ticker=new_instrument.ticker,
-                )
-                .returning(InstrumentOrm)
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_instrument(
+        self, new_instrument: Instrument
+    ) -> Instrument:
+        result = await self.db_session.scalars(
+            insert(InstrumentOrm)
+            .values(
+                name=new_instrument.name,
+                ticker=new_instrument.ticker,
             )
-            return Instrument.from_orm(result.one())
+            .returning(InstrumentOrm)
+        )
+        return Instrument.from_orm(result.one())
 
     @staticmethod
-    async def check_has_in_database(ticker: str) -> bool:
-        async with async_session_factory() as session:
-            query = select(InstrumentOrm.ticker).filter(
-                InstrumentOrm.ticker == ticker
-            )
-            result = await session.execute(query)
-            if result.first() is None:
-                return False
-            return True
+    async def check_has_in_database(self, ticker: str) -> bool:
+        query = select(InstrumentOrm.ticker).filter(
+            InstrumentOrm.ticker == ticker
+        )
+        result = await self.db_session.execute(query)
+        if result.first() is None:
+            return False
+        return True
 
     @staticmethod
-    async def get_instrument_list():
-        async with async_session_factory() as session:
-            query = select(InstrumentOrm)
-            result = await session.execute(query)
-            return [Instrument.from_orm(x) for x in result.scalars().all()]
+    async def get_instrument_list(self):
+        query = select(InstrumentOrm)
+        result = await self.db_session.execute(query)
+        return [Instrument.from_orm(x) for x in result.scalars().all()]
